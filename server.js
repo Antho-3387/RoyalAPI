6325f48e4c52d5cb009751d5d3aba90ddbbcf375
root <root@PCdeHash.localdomain>
2026-01-28 16:41:42 +0100

    2nde push

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Royal API is running");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server running on port " + port));
